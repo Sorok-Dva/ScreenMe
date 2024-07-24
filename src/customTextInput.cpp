e70717210092a9723d406fafc65b2b2b79485238
Сорок два <sorokdva.developer@gmail.com>
2024-07-24 16:56:54 +0200

    chore: installer

--- src/customTextInput.cpp
+++
@@ -1,9 +0,0 @@
-#include "include/customTextEdit.h"
-
-CustomTextEdit::CustomTextEdit(QWidget* parent)
-    : QTextEdit(parent) {}
-
-void CustomTextEdit::focusOutEvent(QFocusEvent* event) {
-    emit focusOut();
-    QTextEdit::focusOutEvent(event);
-}
